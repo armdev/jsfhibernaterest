2fc7f1d0acffab73f8ed9f91705099d0d8ee28e1
Home <Home@armdev.ubuntu.com>
2016-02-27 12:31:25 +0400

    JSF, Hibernate, Spring , REST app

---
+++ hibernate-backend/src/main/java/com/project/services/UserService.java
@@ -0,0 +1,30 @@
+package com.project.services;
+
+import com.project.entities.User;
+
+import java.util.List;
+
+/**
+ *
+ * @author Home
+ */
+public interface UserService {
+
+    public Long getUsersCount();
+     
+    public List<User> getUserList(Integer start, Integer max);
+
+    public Long saveUser(User user);
+
+    public boolean deleteUser(Long id);
+
+    public boolean updateUser(User user);
+
+    public User getUserById(Long id);
+
+    public User loadUser(Long id);
+
+    public User getUser(Long id);
+
+    public User getUserByEmail(String email);
+}
